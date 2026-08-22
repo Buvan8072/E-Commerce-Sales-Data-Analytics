2d32251784f8af66be18b36bb1207f4091418b53
BHUVANESH S <92632377+Buvan8072@users.noreply.github.com>
2026-08-22 19:42:04 +0530

    Remove duplicate Python files

--- 03_eda.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-df = pd.read_csv("Data/superstore.csv")
-print("Total Sales:",df["Sales"].sum())
-print("Total Profit:",df["Profit"].sum())
-print("Total Orders:",df["Order ID"].nunique())
-print("Average Sales:",df["Sales"].mean())
