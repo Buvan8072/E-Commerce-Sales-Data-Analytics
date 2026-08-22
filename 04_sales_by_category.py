a3327197ba0dc327b952233f5ee7a15be9be86af
BHUVANESH S <92632377+Buvan8072@users.noreply.github.com>
2026-08-22 19:42:25 +0530

    Remove duplicate Python files

--- 04_sales_by_category.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df=pd.read_csv("Data/superstore.csv")
-sales_by_category = df.groupby("Category")["Sales"].sum()
-print("===== Sales by Category =====")
-print(sales_by_category)
